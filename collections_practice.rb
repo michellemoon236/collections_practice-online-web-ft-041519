e4a6504852f277af4d2215f7970a6cf208ffc46c
Michelle Moon <michellemoon236@gmail.com>
2019-04-15 21:55:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,6 +49,9 @@
   
   def add_s (array)
     #Add an "s" to each word in the array except for the 2nd element in the array (FAILED - 9)
-    if array.index
+    when array.index[1]
+      array
+    else
+      
   end
 
