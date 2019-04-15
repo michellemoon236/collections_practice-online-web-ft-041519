7e92cdcb36df43a5460e1ee0b03265aec95355e4
Michelle Moon <michellemoon236@gmail.com>
2019-04-15 21:35:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,19 @@
+  def sort_array_asc
+    should return an array sorted in ascending order (FAILED - 1)
+  def sort_array_desc
+    should return an array sorted in descending order (FAILED - 2)
+  def sort_array_char_count
+    should return an array in ascending order sorted by the number of characters in the string (FAILED - 3)
+  def swap_elements
+    swap the second and third elements of an array (FAILED - 4)
+  def reverse_array
+    reverse the order of an array of integers (FAILED - 5)
+  def kesha_maker
+    taking an array as an input, change the 3rd character of each element to a dollar sign. (FAILED - 6)
+  def find_a
+    find all words that begin with "a" in the following array (FAILED - 7)
+  def sum_array
+    sum all the numbers in the following array (FAILED - 8)
+  def add_s
+    Add an "s" to each word in the array except for the 2nd element in the array (FAILED - 9)
 
