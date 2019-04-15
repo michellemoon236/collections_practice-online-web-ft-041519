7707d6b441e66feaa45c059157820a1025b9bb8a
Michelle Moon <michellemoon236@gmail.com>
2019-04-15 21:48:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,7 +29,8 @@
   
   def kesha_maker (array)
     #taking an array as an input, change the 3rd character of each element to a dollar sign. (FAILED - 6)
-      
+    array.collect do |string|
+      string
   end
   
   def find_a (array)
